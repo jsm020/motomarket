0f16449cea45570d1a75dee509254ecadc4f94bc
jsm020 <javohirxikmatillaev@gmail.com>
2024-02-19 05:06:29 -0800

    v3.3

--- data/config.py
+++ data/config.py
@@ -3,18 +3,28 @@ from environs import Env
 # environs kutubxonasidan foydalanish
 env = Env()
 env.read_env()
+ADMINS="6484918914"
+BOT_TOKEN="6773234732:AAGNxsZio1EkZL7eK8vIYftjxs7aepUBgTQ"
+ip="localhost"
+DB_USER="telegram_bot_db_kehw_user"
+DB_PASS="Sm9B8gp85QZr7EKCcUZuKY0hRWFxkfkL"
+DB_NAME="telegram_bot_db_kehw"
+DB_HOST="dpg-cn9ik3ocmk4c73a09au0-a.oregon-postgres.render.com"
+PROVIDER_TOKEN_PAYME="371317599:TEST:1707397043657"
+PROVIDER_TOKEN_CLICK="398062629:TEST:999999999_F91D8F69C042267444B74CC0B3C747757EB0E065"
+PROVIDER_TOKEN_TRANZO="410694247:TEST:1b2f0b01-5e64-4bae-89e2-c780e07e9789a"
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
-PROVIDER_TOKEN_PAYME = env.str("PROVIDER_TOKEN_PAYME")
-PROVIDER_TOKEN_CLICK = env.str("PROVIDER_TOKEN_CLICK")
-PROVIDER_TOKEN_TRANZO = env.str("PROVIDER_TOKEN_TRANZO")
+# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+# IP = env.str("ip")  # Xosting ip manzili
+# PROVIDER_TOKEN_PAYME = env.str("PROVIDER_TOKEN_PAYME")
+# PROVIDER_TOKEN_CLICK = env.str("PROVIDER_TOKEN_CLICK")
+# PROVIDER_TOKEN_TRANZO = env.str("PROVIDER_TOKEN_TRANZO")
 
-# 6484918914
+# # 6484918914
 
-DB_USER = env.str("DB_USER")
-DB_PASS = env.str("DB_PASS")
-DB_NAME = env.str("DB_NAME")
-DB_HOST = env.str("DB_HOST")
+# DB_USER = env.str("DB_USER")
+# DB_PASS = env.str("DB_PASS")
+# DB_NAME = env.str("DB_NAME")
+# DB_HOST = env.str("DB_HOST")
